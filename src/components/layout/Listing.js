d5bc07f12034e69249490bd89122b5d50c06c517
Alexandra Spalato <alexaspalato@gmail.com>
2021-10-21 13:30:53 +0200

    buttons text size

--- src/components/layout/Listing.js
+++ src/components/layout/Listing.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react"
-import clsx from "clsx"
-import { Image } from "../images"
-import { Link } from "gatsby"
-import Collapse from "@kunukn/react-collapse"
-import { FaChevronDown } from "react-icons/fa"
-import { useBucketList } from "../../lib/hooks/useBucketList"
-import { Button } from ".."
-import { Price } from "../Price"
-import { ListingBottomInfo } from "./ListingBottomInfo"
-import { AddToBlButton } from "../bucket-list/AddToBlButton"
+import React, { useState } from "react";
+import clsx from "clsx";
+import { Image } from "../images";
+import { Link } from "gatsby";
+import Collapse from "@kunukn/react-collapse";
+import { FaChevronDown } from "react-icons/fa";
+import { useBucketList } from "../../lib/hooks/useBucketList";
+import { Button } from "..";
+import { Price } from "../Price";
+import { ListingBottomInfo } from "./ListingBottomInfo";
+import { AddToBlButton } from "../bucket-list/AddToBlButton";
 
 export const Listing = ({
   item,
@@ -22,18 +22,18 @@ export const Listing = ({
   nested = false,
   ...props
 }) => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
-  let { title, intro, externalLink, link } = item || {}
+  let { title, intro, externalLink, link } = item || {};
 
   let { featuredImage, uri, commonDataAttributes, customDataAttributes } = link
     ? link[0] || {}
-    : item || {}
+    : item || {};
 
-  let { standfirst, country } = commonDataAttributes || {}
-  const blItem = item.link ? item.link[0] : item
+  let { standfirst, country } = commonDataAttributes || {};
+  const blItem = item.link ? item.link[0] : item;
 
-  const { addToBl, removeFromBl, isAdded } = useBucketList(blItem)
+  const { addToBl, removeFromBl, isAdded } = useBucketList(blItem);
 
   let {
     starRating,
@@ -47,17 +47,17 @@ export const Listing = ({
     city,
     region,
     summaryBio,
-  } = customDataAttributes || {}
+  } = customDataAttributes || {};
 
-  profile = profile ? profile : "full"
+  profile = profile ? profile : "full";
 
-  website = externalLink ? externalLink : website
+  website = externalLink ? externalLink : website;
   uri =
     profile === "full" || pts || itinerary || writer || roundUp || nested
       ? uri
       : website
       ? website
-      : "#"
+      : "#";
 
   const img = featuredImage ? (
     featuredImage.node.localFile ? (
@@ -82,7 +82,7 @@ export const Listing = ({
     <div className="flex items-center justify-center col-span-1 bg-veryLightGold w-[249px] h-[166px] text-grey4 ">
       No Image
     </div>
-  )
+  );
   return (
     <div className={clsx("shadow-listing", "p-2 pr-3 mb-5", className)}>
       <div className={clsx("flex justify-between")} {...props}>
@@ -148,7 +148,7 @@ export const Listing = ({
           writer ||
           nested ||
           roundUp ? (
-            <Link to={uri} className="!text-[11px] btn h-10">
+            <Link to={uri} className="h-10 text-xs btn">
               {writer ? "Read more" : "Review"}
             </Link>
           ) : (
@@ -160,6 +160,7 @@ export const Listing = ({
                 href={website}
                 target="_blank"
                 rel="noopener noreferrer"
+                className="text-xs"
               >
                 Website
               </Button>
@@ -171,8 +172,8 @@ export const Listing = ({
               secondary
               // className="leading-none !text-[11px]"
               onClick={(e) => {
-                e.preventDefault()
-                setOpen(!open)
+                e.preventDefault();
+                setOpen(!open);
               }}
               css={{
                 "&:hover": {
@@ -208,5 +209,5 @@ export const Listing = ({
         </Collapse>
       )}
     </div>
-  )
-}
+  );
+};
