7fb00e734a99e5190021020afefeb418c5606a8c
Alexandra Spalato <alexaspalato@gmail.com>
2021-12-13 14:56:14 +0000

    remove search from home and search

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -29,28 +29,15 @@ export const Header = () => {
         )}
         <div className="flex items-center space-x-5">
           <Menu className="hidden lg:flex" />
+          {path !== "/" && !path?.includes("search") && path !== "/404" && (
           <HeaderMobileSearch className="hidden mr-5 lg:block xl:hidden" />
+          )}
         </div>
         <div className={clsx("lg:hidden", "flex space-x-5")}>
           {path !== "/" && !path?.includes("search") && path !== "/404" && (
             <HeaderMobileSearch />
           )}
-
-          <div className="flex items-center space-x-5">
-            <Menu className="hidden lg:flex" />
-            {path !== "/" && !path?.includes("search") && path !== "/404" && (
-            <HeaderMobileSearch className="hidden mr-5 lg:block xl:hidden" />
-            )}
-          </div>
-          <div className={clsx("lg:hidden", "flex space-x-5")}>
-            {path !== "/" && !path?.includes("search") && path !== "/404" && (
-              <HeaderMobileSearch />
-            )}
-            <MobileMenu />
-          </div>
-
           <MobileMenu />
-
         </div>
       </div>
       <Toaster />
