1f40a9dc2178a6b13bc545ac3000f3f6edac2639
Alexandra Spalato <alexaspalato@gmail.com>
2021-12-14 16:32:27 +0000

    checkboxes correction

--- src/components/my-components/CustomRefinementList.js
+++ src/components/my-components/CustomRefinementList.js
@@ -143,7 +143,7 @@ const CustomRefinementList = (props) => {
                         value={staticItem.value}
                         checked={isRefined}
                         className={clsx(
-                          "input-item border-2 rounded-none text-gold form-checkbox border-grey2 w-5 h-5 mr-4",
+                          "input-item border-2 rounded-none text-gold form-checkbox border-grey2 w-5 h-5 mr-4 ml-1 sm:ml-0",
                           {
                             "cursor-not-allowed ": !count,
                           }
