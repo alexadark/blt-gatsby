ecd3773e509b51663cdade00e2da702d961b3c17
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2022-01-08 19:43:50 +0530

    Clear filter url update

--- src/components/my-components/ClearAllFilters.js
+++ src/components/my-components/ClearAllFilters.js
@@ -1,10 +1,18 @@
 import { connectCurrentRefinements } from "react-instantsearch-dom";
 import React from "react";
 import { Button } from "../ui-components/Button";
+import isBrowser from "../../utils/isBrowser";
 const ClearAllFilters = ({ items, refine }) => {
   return (
     <Button
-      onClick={() => refine(items)}
+      onClick={() => {
+        refine(items);
+        if (isBrowser()) {
+          const url = new URL(window.location.href);
+          url.searchParams.set("tab", "All");
+          window.history.pushState(null, "", url);
+        }
+      }}
       disabled={!items.length}
       secondary
       small
