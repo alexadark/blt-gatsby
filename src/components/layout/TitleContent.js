911af9455ab0259508bf868cfa5f877b3fe565f8
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2022-02-14 17:46:27 +0530

    PHASE 2 Read more..

--- src/components/layout/TitleContent.js
+++ src/components/layout/TitleContent.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { Typo } from "../ui-components"
+import React, { useState } from "react";
+import { Typo } from "../ui-components";
 
 export const TitleContent = ({
   title = null,
@@ -7,9 +7,14 @@ export const TitleContent = ({
   className = "",
   ...props
 }) => {
+  let [isOpen, setIsOpen] = useState(false);
+  let contentArray = content?.split(" ") ?? [];
+  let contentArrayLength = contentArray?.length;
+  let first75Words = contentArray?.slice(0, 75);
   if (!content) {
-    return null
+    return null;
   }
+
   return (
     <div {...props}>
       {title && (
@@ -17,7 +22,38 @@ export const TitleContent = ({
           {title}
         </Typo>
       )}
-      <div className="prose-lg" dangerouslySetInnerHTML={{ __html: content }} />
+      {contentArrayLength > 75 ? (
+        <div>
+          <div
+            className="prose-lg"
+            dangerouslySetInnerHTML={{
+              __html: isOpen ? content : first75Words.join(" "),
+            }}
+          />
+          <div className="buttons flex justify-center mt-2 sm:mt-0 sm:justify-start">
+            {isOpen ? (
+              <button
+                onClick={() => setIsOpen(false)}
+                className="showmore border-2 rounded border-lightBlue text-gray-800 text-lg p-2 px-6 sm:p-0 sm:border-0  sm:inline-block sm:text-blueLink font-semibold"
+              >
+                Show less
+              </button>
+            ) : (
+              <button
+                onClick={() => setIsOpen(true)}
+                className="loadmore border-2 rounded border-lightBlue text-gray-800 text-lg p-2 px-6 sm:p-0 sm:border-0  sm:inline-block sm:text-blueLink font-semibold"
+              >
+                Read more
+              </button>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div
+          className="prose-lg"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
